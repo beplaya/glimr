0331c77fead344be82395cf675586ec99784d018
Grant James <grant.james@cardinalhealth.com>
2016-02-03 18:48:50 -0500

    GLIMR-1 commit id and author tested

--- glimr/glimr.js
+++ glimr/glimr.js
@@ -1,4 +1,47 @@
+module.exports = function() {
+    var GLIMR = {};
 
+    GLIMR.toEntryObjectsArray = function(logOutput) {
+        var entryTextBlocks = logOutput.split("\ncommit ");
+        var entryObjects = [];
+        var index = 0;
+        for(var i=0; i<entryTextBlocks.length; i++){
+            var entryLines = entryTextBlocks[i];
+            if(entryLines.length===0){
+                continue;
+            }else {
+                var entryObj = GLIMR.createEntry(entryLines);
+                entryObjects[index] = entryObj;
+                index++;
+            }
+        }
+        entryObjects = entryObjects.sort(function(a, b){
+            return a.date.getTime() < b.date.getTime() ? 1 : (a.date.getTime() === b.date.getTime() ? 0 : -1);
+        });
+        return entryObjects;
+    };
 
-console.log("GLIMR");
+    GLIMR.createEntry = function(entryLines) {
+        var entry = {};
+        var lines = entryLines.split("\n");
+        for(var i=0; i<lines.length; i++){
+            var l = lines[i];
+            if(l.length > 0 && !entry.commit) {
+                entry.commit = l.replace(/commit/g, "").trim();
+            } else if(l.indexOf("Author: ") != -1) {
+                var authorLineSplit = l.split("<");
+                entry.author = {
+                    name: authorLineSplit[0].replace(/:/g, "").replace(/Author/g, "").trim(),
+                    email: authorLineSplit[1].replace(/>/g, "").trim()
+                };
+            } else if(l.indexOf("Date: ") != -1) {
+                entry.date = new Date(l.replace("Date: ", ""));
+            }
+        }
+        entry.message = entryLines.split("Date: ")[1];
+        entry.message = entry.message.substring(entry.message.indexOf("\n\n")+"\n\n".length, entry.message.length).trim();
+        return entry;
+    };
 
+    return GLIMR;
+}
--- tests/index.js
+++ tests/index.js
@@ -1,10 +1,38 @@
 
 var superagent = require('superagent'),
-    expect = require('expect.js');
+expect = require('expect.js'),
+fs = require('fs'),
+glimr = require(__dirname + '/../glimr/glimr.js')();
 
-describe('test tests', function(){
-      it('should run tests',function(){
-             expect(1).to.equal(1);
-             expect(2).to.equal(2);
-      })
-  });
+var nitLogs = {};
+nitLogs.content = fs.readFileSync(__dirname + "/data/nitlogs.txt").toString();
+nitLogs.logObjects = glimr.toEntryObjectsArray(nitLogs.content);
+
+
+describe('Log Objectifier', function(){
+    it('should objectify logs',function(){
+        expect(nitLogs.logObjects.length).to.equal(363);
+    });
+});
+describe('Log Objectifier', function(){
+    it('should objectify commit ids',function(){
+        expect("0~" + nitLogs.logObjects[0].commit).to.equal("0~" + "2f236253b3ba306157cff82f5f405d00648d46f0");
+        expect("4~" + nitLogs.logObjects[4].commit).to.equal("4~" + "4ee4f7658f6ef29d585f6a84e7d20acf25cc2c03");
+        var index = (nitLogs.logObjects.length-1);
+        expect(index + "~" + nitLogs.logObjects[index].commit).to.equal(index + "~" + "c316a2cc0daeb74e6079b656f95702a83c39e86d");
+    });
+});
+
+describe('Log Objectifier', function(){
+    it('should objectify commit author name',function(){
+        expect("0~" + nitLogs.logObjects[0].author.name).to.equal("0~TEST AuTHOR0");
+        expect("1~" + nitLogs.logObjects[1].author.name).to.equal("1~T3ST AuTttHOR1");
+    });
+});
+
+describe('Log Objectifier', function(){
+    it('should objectify commit author email address',function(){
+        expect("0~" + nitLogs.logObjects[0].author.email).to.equal("0~author0@email.net");
+        expect("1~" + nitLogs.logObjects[1].author.email).to.equal("1~testa1@thing.com");
+    });
+});
