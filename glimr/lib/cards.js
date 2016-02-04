ca84be4b58e550e6fcdea3a6a8f06687b9079b52
Grant James <grant.james@cardinalhealth.com>
2016-02-04 18:05:23 -0500

    GLIMR-3 finds unique cards

--- glimr/lib/cards.js
+++ glimr/lib/cards.js
@@ -7,13 +7,19 @@ module.exports = function(){
         for(var i=0; i<logObjects.length; i++) {
             var message = logObjects[i].message || "";
             var isMergePull = false;
+            var pullRequestNumber = -1;
             if(message.indexOf(pullRequestSearch) != -1){
                 isMergePull = true;
+                var indexOfPullRequest = message.indexOf(pullRequestSearch);
+                pullRequestNumber = message.substr(indexOfPullRequest);
+                pullRequestNumber = pullRequestNumber.substr(pullRequestSearch.length);
+                pullRequestNumber = pullRequestNumber.substr(0, pullRequestNumber.indexOf(" "));
             }
             var messageLines = message.split("\n");
             for(var mli=0; mli<messageLines.length; mli++){
                 messageLines[mli] = messageLines[mli].trim();
-                if(messageLines[mli].indexOf(search) == 0){
+                if(messageLines[mli].indexOf(search) == 0 || (messageLines[mli].indexOf(search) != -1
+                        && messageLines[mli].indexOf(pullRequestSearch) != -1)){
                     var spaceIndex = messageLines[mli].indexOf(" ");
                     var issueKey = messageLines[mli].substr(0, spaceIndex).trim();
                     var split = issueKey.split("-");
@@ -26,10 +32,10 @@ module.exports = function(){
                                     key : issueKey,
                                     number : issueNumber,
                                     projectKey : projectKey,
-                                    commits : [ {logObject: logObjects[i], isMergePull : isMergePull}]
+                                    commits : [ {logObject: logObjects[i], isMergePull : isMergePull, pullRequestNumber: pullRequestNumber}]
                                 });
                             } else {
-                                cards[existingCardIndex].commits.push({logObject: logObjects[i], isMergePull : isMergePull});
+                                cards[existingCardIndex].commits.push({logObject: logObjects[i], isMergePull : isMergePull, pullRequestNumber: pullRequestNumber});
                             }
                         }
                     }
