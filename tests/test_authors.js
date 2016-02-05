5e238410785c05c3cd4f8011dcc25cc0c8416131
Grant James <grant.james@cardinalhealth.com>
2016-02-04 22:04:40 -0500

    GLIMR-7 unique authors and author activity

---
+++ tests/test_authors.js
@@ -0,0 +1,18 @@
+var expect = require('expect.js'),
+fs = require('fs'),
+glimrBuild = require(__dirname + '/../glimr/glimr_build.js')();
+var nitLogs = {};
+nitLogs.content = fs.readFileSync(__dirname + "/data/nitlogs.txt").toString();
+nitLogs.logObjects = glimrBuild.toLogObjectsArray(nitLogs.content, true);
+nitLogs.authors = glimrBuild.authors.findUniqueAuthors(nitLogs.logObjects);
+
+describe('cards', function(){
+
+    it('should a list of unique authors',function(){
+        expect(nitLogs.authors.length).to.equal(5);
+        //for(var i=0; i<nitLogs.authors.length; i++) {
+        //    console.log(nitLogs.authors[i].activity.contribution);
+        //}
+    });
+
+});
