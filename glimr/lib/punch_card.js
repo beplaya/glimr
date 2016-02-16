0c985f518ba9576368f5b9564482fb0374cc0c01
Grant James <grant.james@cardinalhealth.com>
2016-02-15 19:23:32 -0500

    GLIMR-11 wip

---
+++ glimr/lib/punch_card.js
@@ -0,0 +1,23 @@
+module.exports = function() {
+    var P = {};
+    /*
+        {
+        "daily" : [
+         "sunday" : {
+             numberOfCommits : INT,
+             numberOffHoursCommits : INT,
+             numberOfcards : INT,
+             numberOfAuthors : INT
+          } ]
+        }
+    */
+
+    P.createPunchcard = function(logObjects) {
+        var days = ["sunday", "monday", "tuesday", "wednesday"];
+        for(var i=; i<logObjects.length; i++)
+    };
+
+    P.
+
+    return P
+};
